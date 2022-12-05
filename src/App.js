c881c0622efa4cb69e971af22cc623c625c73119
Ahmed Iqbal Bhatti <ahmad786687iqbal@gmail.com>
2022-12-05 10:29:31 +0500

    Complete React YouTube Clone App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {ChannelDetail, Feed, NavBar, SearchFeed, VideoDetail} from "./components";
+import {Box} from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Box sx={{backgroundColor: '#000'}}>
+                <NavBar/>
+                <Routes>
+                    <Route path='/' exact element={<Feed/>}/>
+                    <Route path='/video/:id' exact element={<VideoDetail/>}/>
+                    <Route path='/channel/:id' exact element={<ChannelDetail/>}/>
+                    <Route path='/search/:searchTerm' exact element={<SearchFeed/>}/>
+                </Routes>
+            </Box>
+        </BrowserRouter>
+    );
 }
 
 export default App;
